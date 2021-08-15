10867e892ed3c3f47a3a26293c27ada29823fd48
lzn747 <lzn747@gmail.com>
2021-08-15 11:54:36 +0300

    Forms created.

---
+++ AuthApp/forms.py
@@ -0,0 +1,71 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class LoginForm(forms.ModelForm):
+    password = forms.Charfield(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Kasutaja nimi'
+        self.fields['password'].label = 'Parool'
+
+    def clean(self):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        user = User.objects.filter(username=username).first()
+        if not user:
+            raise forms.ValidationError(f'Kasutaja {username} ei ole leitud')
+        if not user.check_password(password):
+            raise forms.ValidationError('Vale parool')
+        return self.cleaned_data
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    phone = forms.CharField(required=False)
+
+    address = forms.CharField(required=False)
+
+    email = forms.EmailField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Kasutaja nimi'
+        self.fields['confirm_password'].label = 'Kinnitage parool'
+        self.fields['phone'].label = 'Telefoni number'
+        self.fields['address'].label = 'Aadres'
+        self.fields['email'].label = 'Email'
+        self.fields['first_name'].label = 'Eesnimi'
+        self.fields['last_name'].label = 'Perekonnanimi'
+
+    def _clean_email(self):
+        self.fields['email'].label = 'Email'
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Selline email aadres on juba kasutuses')
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError(f'{username} on juba kasutuses')
+
+    def clean(self):
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+        if password != confirm_password:
+            raise forms.ValidationError('Sisesta sama paroolid')
+        return self.cleaned_data
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'confirm_password', 'first_name', 'last_name', 'email', 'phone', 'address']
